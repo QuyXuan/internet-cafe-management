064a16accf7d78df978c66254afa80ebccc8c0d1
mxtruongplayboy <113846053+mxtruongplayboy@users.noreply.github.com>
2023-04-22 11:52:20 +0700

    04/22/2023 trường commit

--- BLL/ComputerBLL.cs
+++ BLL/ComputerBLL.cs
@@ -3,6 +3,8 @@ using DTO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -36,8 +38,8 @@ namespace BLL
                 return computers;
             }
         }
-        //Hàm lấy ra NameType bằng TypeId
-        public string GetNameTypeByTypeId(string typeId)
+        //Hàm lấy ra TypeComputer bằng TypeId
+        public TypeComputer GetTypeComputerByTypeId(string typeId)
         {
             using (var context = new QLNETDBContext())
             {
@@ -50,8 +52,44 @@ namespace BLL
                 {
                     return null;
                 }
-                return typeComputer.NameType;
+                return typeComputer;
             }
+        }
+        //Hàm lấy ra Computer bằng IP
+        public Computer GetComputerByIP(string IP)
+        {
+            using (var context = new QLNETDBContext())
+            {
+                if (context == null)
+                {
+                    return null;
+                }
+                var computer = context.Computers.FirstOrDefault(p => p.IPComputer == IP);
+                if (computer == null)
+                {
+                    return null;
+                }
+                return computer;
+            }
+        }
+        //Hàm lấy IP của máy
+        public string GetLocalIPv4(NetworkInterfaceType _type)
+        {
+            string output = "";
+            foreach (NetworkInterface item in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (item.NetworkInterfaceType == _type && item.OperationalStatus == OperationalStatus.Up)
+                {
+                    foreach (UnicastIPAddressInformation ip in item.GetIPProperties().UnicastAddresses)
+                    {
+                        if (ip.Address.AddressFamily == AddressFamily.InterNetwork)
+                        {
+                            output = ip.Address.ToString();
+                        }
+                    }
+                }
+            }
+            return output;
         }
     }
 }
