1dd285f1e1f114ba50fab2c34fc30a99768ae8e7
mxtruongplayboy <113846053+mxtruongplayboy@users.noreply.github.com>
2023-05-13 20:02:52 +0700

    13/5/2023 trường

--- GUIClient/frmLoginClient.Designer.cs
+++ GUIClient/frmLoginClient.Designer.cs
@@ -233,6 +233,7 @@
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmLoginClient";
+            this.TopMost = true;
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.panelDangNhap.ResumeLayout(false);
             this.panelDangNhap.PerformLayout();
